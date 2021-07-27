f4f4f408c4842b8d06a08540def2f3eb34d9eefb
Anderson Alencar <anderson.alencar@kobe.io>
2021-07-26 23:35:27 -0300

    add gitignore and start project

---
+++ EmbedCoordinator/EmbedCoordinator/EmbedCoordinator.h
@@ -0,0 +1,18 @@
+//
+//  EmbedCoordinator.h
+//  EmbedCoordinator
+//
+//  Created by Anderson on 26/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmbedCoordinator.
+FOUNDATION_EXPORT double EmbedCoordinatorVersionNumber;
+
+//! Project version string for EmbedCoordinator.
+FOUNDATION_EXPORT const unsigned char EmbedCoordinatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmbedCoordinator/PublicHeader.h>
+
+
